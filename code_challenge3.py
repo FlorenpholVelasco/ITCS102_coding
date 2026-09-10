4c577ea36056e2e2b2450bcea40454dabf6de8cf
FlorenpholVelasco <florenpholvelasco1@gmail.com>
2026-09-10 11:17:28 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,18 @@
+#code_challenge3
+
+
+name = input("input name --> ")
+item = input("input item --> ")
+is_Itemfragile = bool(input("fragile"))
+
+
+print("sender is, ", name," The item is, ", item)
+
+
+if is_Itemfragile == True:
+
+         print("fragile")
+
+else:
+
+         print("not fragile")
